bc347d6518d03ef2e5688bb78e0907a6ea1f0427
yosefflanes <yosefflanes@gmail.com>
2026-07-10 15:02:12 +0700

    Feat: add button login and register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,49 @@
 import { Link } from "react-router-dom";
-import { Button } from "@/components/ui/button"
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const Navbar = () => {
-    return (
-        <nav className="w-full h-16 px-10 flex items-center justify-between text-primary bg-red-500">
-            <div>
-                <Link to="/"><h2 className="font-bold text-xl">EduPro</h2></Link>
-            </div>
-            <div className="flex gap-8">
-                <Link to="/">Home</Link>
-                <Link to="/courses">Courses</Link>
-                <Link to="/dashboard">Dashboard</Link>
-            </div>
-            <div className="flex gap-6">
-                <Button>Login</Button>
-                <Link to="/register">Register</Link>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="w-full h-16 px-10 flex items-center justify-between text-primary">
+      <div>
+        <Link to="/">
+          <h2 className="font-bold text-xl">EduPro</h2>
+        </Link>
+      </div>
+      <div className="flex gap-8">
+        <Link to="/">Home</Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </div>
+      <Stack direction="row" spacing={2}>
+        <Button
+          variant="text"
+          sx={{
+            color: "var(--color-secondary)",
+            fontSize: "14px",
+            fontFamily: '"Poppins", sans-serif',
+            "&:hover": {
+                borderRadius: "8px",
+            }
+          }}
+        >
+          Login
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            color: "#ffffff",
+            fontSize: "14px",
+            fontFamily: '"Poppins", sans-serif',
+            borderRadius: "8px",
+            backgroundColor: "var(--color-primary)",
+          }}
+        >
+          Register
+        </Button>
+      </Stack>
+    </nav>
+  );
+};
 
 export default Navbar;
