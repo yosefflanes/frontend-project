45abfc267ef5cbc04ceda5baef07c048b0758212
yosefflanes <yosefflanes@gmail.com>
2026-07-14 10:59:04 +0700

    feat: protect Dashboard and Users page to Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
 
@@ -12,10 +14,27 @@ function App() {
       <Navbar />
 
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        
+
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+
+          <Route 
+          path="/users" 
+          element={
+            <ProtectedRoute>
+              <Users />
+            </ProtectedRoute>
+          } />
       </Routes>
     </>
   );
