987bc76820dec41092f609edfaf1ca075ae39fd3
yosefflanes <yosefflanes@gmail.com>
2026-07-16 14:12:00 +0700

    feat: add CardFooter component for displaying instructor information and rating

---
+++ src/components/molecules/CardFooter.jsx
@@ -0,0 +1,20 @@
+import { MdStarRate } from "react-icons/md";
+
+const CardFooter = ({ instructorName, rating }) => {
+  return (
+    <div className="p-5 pt-0 border-t border-gray-50 flex justify-between items-center text-xs mt-4">
+      <div>
+        <p className="text-subtitle">Instruktur</p>
+        <p className="font-semibold text-pretty">
+          {instructorName || "Anonim"}{" "}
+        </p>
+      </div>
+      <div className="text-right">
+        <p className="text-subtitle"><MdStarRate className="text-yellow-400" />Rating</p>
+        <p className="font-bold text-primary">{rating} / 10</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardFooter;
